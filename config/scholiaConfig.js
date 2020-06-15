4cb3759879e759f13bb105ed03f1bd94f771b630
Ubuntu <ubuntu@ip-172-31-24-169.ec2.internal>
2020-06-15 14:40:12 +0000

    improving nginx proxy capabilities

--- config/scholiaConfig.js
+++ config/scholiaConfig.js
@@ -1,11 +1,11 @@
 // A $( document ).ready() block.
 
-//      webserviceURL: 'https://www.wikidata.org/w/api.php',
-//      SPARQLEndPointURL: 'https://query.wikidata.org/sparql'
 
 var scholiaConfig = {
-      webserviceURL: 'http://proxy-scholiaproxy7.prod.openrisknet.org//w/api.php',
-      SPARQLEndPointURL: 'http://proxy-scholiaproxy7.prod.openrisknet.org/sparql'
+      webserviceURL: 'https://www.wikidata.org/w/api.php',
+      SPARQLEndPointURL: 'https://query.wikidata.org/sparql'
+//      webserviceURL: 'http://proxy-scholiaproxy7.prod.openrisknet.org//w/api.php',
+//      SPARQLEndPointURL: 'http://proxy-scholiaproxy7.prod.openrisknet.org/sparql'
 }
 
 
